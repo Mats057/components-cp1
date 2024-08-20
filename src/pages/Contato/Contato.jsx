8745e9692537e9e4531ab6a1ff3abd5a7e8b8e49
Matheus Zanutin <119885089+Mats057@users.noreply.github.com>
2024-08-20 17:33:31 -0300

    contato

--- src/pages/Contato/Contato.jsx
+++ src/pages/Contato/Contato.jsx
@@ -1,12 +1,34 @@
-import { Header } from '../../components/Header/Header'
-import './Contato.css'
+import "./Contato.css";
+import Twitter from "../../assets/twitter.png";
+import Instagram from "../../assets/instagramIcone.png";
+import Discord from "../../assets/discord.png";
 
-function Contato(){
-    return (
-        <main>
-
-        </main>
-    )   
+function Contato() {
+  return (
+    <main className="contato">
+      <section>
+        <h1>Dúvidas e suporte, entre em contato:</h1>
+        <div className="redes">
+            <a href="https://x.com" target="_BLANK"><img src={ Twitter } alt="Twitter" /></a>
+            <a href="https://instagram.com" target="_BLANK"><img src={ Instagram } alt="Instagram" /></a>
+            <a href="https://discord.com" target="_BLANK"><img src={ Discord } alt="Discord" /></a>
+        </div>
+      </section>
+      <section>
+        <form>
+          <div className="inputs">
+            <label htmlFor="nome">Nome:
+            <input type="text" id="nome" name="nome" required /></label>
+            <label htmlFor="email">Email:
+            <input type="email" id="email" name="email" required /></label>
+            <label htmlFor="mensagem">Mensagem:</label>
+            <textarea id="mensagem" name="mensagem" rows={6} required></textarea>
+          </div>
+          <button type="submit">Enviar</button>
+        </form>
+      </section>
+    </main>
+  );
 }
 
-export default Contato
+export default Contato;
